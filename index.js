5c5d2144f1227b9a92223b4a7b5cd0d1f5b24a38
nKorous <nathaniel.korous@gmail.com>
2021-06-15 13:08:21 -0600

    added another customer

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const app = express()
-const PORT = process.env.PORT || 8888
+const app = express();
+const PORT = process.env.PORT || 8888;
 
-const customers = require('./customers.js')
+const customers = require("./customers.js");
 
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(cors())
-app.use(bodyParser.json())
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-app.use('/api', customers)
+app.use("/api", customers);
